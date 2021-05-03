4e51d20803789f7ec19051eb9688ed139c916e2d
Pedro-m-Duarte <79157662+Pedro-m-Duarte@users.noreply.github.com>
2021-05-02 23:59:08 -0300

    Create 1002.c

---
+++ 1002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    
+    double R,A,n;
+    
+    scanf("%lf",&R);
+    n = 3.14159;
+    A = 3.14159 * (R * R);
+    printf("A=%.4f\n", A);
+    
+    return 0;
+}
